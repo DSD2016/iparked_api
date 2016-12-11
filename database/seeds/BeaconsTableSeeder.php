a10348dd050fe8e26fddf4357b03649fba066c56
Mihovil2904 <mihovil.dujmenovic@fer.hr>
2016-12-11 16:38:57 +0100

    Update BeaconsTableSeeder.php

--- database/seeds/BeaconsTableSeeder.php
+++ database/seeds/BeaconsTableSeeder.php
@@ -30,7 +30,7 @@ class BeaconsTableSeeder extends Seeder
         ]);
         
         DB::table('beacons')->insert([
-            'floor_id' => 2,
+            'floor_id' => 1,
             'name' => 'beacon3',
             'latitude' => 45.4,
             'longitude' => 15.4,
@@ -39,8 +39,8 @@ class BeaconsTableSeeder extends Seeder
         ]);
         
         DB::table('beacons')->insert([
-            'floor_id' => 1,
-            'name' => 'beacon2',
+            'floor_id' => 2,
+            'name' => 'beacon4',
             'latitude' => 45.6,
             'longitude' => 15.6,
             'minor_number' => 4,
